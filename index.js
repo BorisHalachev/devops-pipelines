c8153e8374faa7422a7e3f0e30944164e0444219
Boris <aepbobi023@gmail.com>
2026-03-27 20:10:30 +0200

    feat : add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, DevOps Pipelines!");
+console.log("Im learining how to set up CI/CD pipelines using gitHub actions.");
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Starting test script...");
+setTimeout(() => {
+    console.log("Test script completed.");
+}, 3000);
+
+console.log("Test Complete!");
